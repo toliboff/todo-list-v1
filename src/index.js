3cf067d6b1249e6cab08d1f698f50fefc3501b2c
Tolibjon <tolibjohn@gmail.com>
2021-07-13 18:16:43 +0500

    Add transition effects

--- src/index.js
+++ src/index.js
@@ -26,11 +26,20 @@ const todoList = [
 ];
 
 function renderList(arr) {
-  list.innerHTML = arr.sort((a, b) => +a.index - +b.index).map((item) => `<li class="flex-row todo">
+  list.innerHTML = arr.sort((a, b) => +a.index - +b.index).map((item) => `<li class="flex-row todo" data-id="${item.index}">
                                       <input type="checkbox" class="checkbox" id="${item.index}"  ${item.completed ? 'checked' : ''}>
-                                      <input type="text" value="${item.description}" class="todo-text ${item.completed ? 'completed' : ''}">
+                                      <input type="text" value="${item.description}" data-index="${item.index}" class="todo-text ${item.completed ? 'completed' : ''}">
                                       <i class="fas fa-ellipsis-v dots"></i>
                                       </li>`).join('');
+  document.querySelectorAll('.todo-text').forEach((text) => {
+    text.addEventListener('focus', (event) => {
+      document.querySelectorAll('.todo').forEach((t) => {
+        t.style.backgroundColor = '#fff';
+      });
+      event.target.parentNode.style.backgroundColor = '#fea';
+    });
+  });
+
   const checkboxes = document.querySelectorAll('.checkbox');
   checkboxes.forEach((chbox) => {
     chbox.addEventListener('change', (event) => {
