be2c91140a1a298a249a4a245b0642f61ea9c76e
Tolibjon <tolibjohn@gmail.com>
2021-07-15 20:52:07 +0500

    Add addTodo function for adding new todo

---
+++ src/addClearTodo.js
@@ -0,0 +1,17 @@
+import renderList from './renderList.js';
+import { getFromStorage, saveToStorage } from './storage.js';
+
+export function addTodo(event) {
+  event.preventDefault();
+  const input = event.target.children[0];
+  const todos = getFromStorage('TodoList');
+  todos.push({ index: todos.length, description: input.value, completed: false });
+  input.value = '';
+  saveToStorage('TodoList', todos);
+  renderList(todos);
+}
+
+export function removeTodo(event) {
+  console.log('removeTodo');
+}
+
